69fc3b6ed39af579728d12cd01bf9a32bc353da6
shikharchopra <shikhar.chopra@nagarro.com>
2023-01-30 10:32:33 +0530

    updated sql file, added solution for Q2

--- Solutions.sql
+++ Solutions.sql
@@ -46,4 +46,15 @@ insert into customer_orders values(1,100,cast('2022-01-01' as date),2000),(2,200
 ,(4,100,cast('2022-01-02' as date),2000),(5,400,cast('2022-01-02' as date),2200),(6,500,cast('2022-01-02' as date),2700)
 ,(7,100,cast('2022-01-03' as date),3000),(8,400,cast('2022-01-03' as date),1000),(9,600,cast('2022-01-03' as date),3000);
 
-select * from customer_orders
+select * from customer_orders;
+
+with cte as
+(
+	select customer_id, order_date, row_number() over(partition by customer_id order by customer_id) rn
+	from customer_orders
+)
+select order_date, sum(case when rn = 1 then 1 else 0 end) new_customer_count,
+sum(case when rn > 1 then 1 else 0 end) repeat_customer_count
+from cte
+group by order_date
+order by order_date
